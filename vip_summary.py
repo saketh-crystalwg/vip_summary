ff333288cdc73d309276bad7c101b57ec10c7264
saketh-crystalwg <saketh@crystalwg.com>
2024-01-31 17:11:16 +0530

    Addition of Lina to to_list

--- vip_summary.py
+++ vip_summary.py
@@ -359,6 +359,6 @@ body = f"Hi,\n\n Attached contains list of VIP customer Segments as of {date_1}\
 sender = "sakethg250@gmail.com"
 recipients = ["saketh@crystalwg.com","alberto@crystalwg.com",\
              "isaac@crystalwg.com","ron@crystalwg.com","sebastian@crystalwg.com",\
-             "rafael@crystalwg.com","sandra@crystalwg.com","erika@crystalwg.com","camila@crystalwg.com"]
+             "rafael@crystalwg.com","sandra@crystalwg.com","erika@crystalwg.com","camila@crystalwg.com","lina.betcoco@gmail.com"]
 password = "xjyb jsdl buri ylqr"
 send_mail(sender, recipients, subject, body, "smtp.gmail.com", 465,sender,password)
